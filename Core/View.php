f17d39ce114f73a83eb19e70dc321eda7a61d552
Ahmet Parlak <ahmet--parlak@outlook.com>
2023-08-01 01:07:20 +0300

    Create basic View structure in Core

---
+++ Core/View.php
@@ -0,0 +1,18 @@
+<?php
+namespace Core;
+
+class Veiw
+{
+    public $content;
+    public function load($viewName, $data = [])
+    {
+        ob_start();
+        extract($data); //Import variables into the current symbol table from an array
+        require BASEDIR . '/App/View/' . $viewName . '.php';
+        $this->content = ob_get_contents();
+        ob_clean();
+
+        return $this->content;
+
+    }
+}
