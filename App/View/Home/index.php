2d31c64ac8f38e402a7b6baee00771c93f8720a7
Ahmet Parlak <ahmet--parlak@outlook.com>
2023-08-01 19:17:49 +0300

    Add JIT mode to Tailwind and update file names
    -Set the mode to "jit" in the tailwind.config.js for just-in-time compilation
    -Renamed input.css and output.css files to tailwind.css for consistency
    -Successfully tested the changes to ensure everything works as expected

--- App/View/Home/index.php
+++ App/View/Home/index.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Patika.dev | Twitter Clone</title>
 
-    <link href="public/dist/output.css" rel="stylesheet">
+    <link href="public/dist/tailwind.css" rel="stylesheet">
 </head>
 
 <body>
 
-    <h1 class="text-3xl font-bold underline">Twitter Clone</h1>
+    <h1 class="text-3xl font-bold underline text-sky-400">Twitter Clone</h1>
 
 </body>
 
