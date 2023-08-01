f9d513120d65801ca649aad849be1567005ea798
Ahmet Parlak <ahmet--parlak@outlook.com>
2023-08-01 16:10:49 +0300

    Add Tailwind CSS for styling
    Details:
    - Integrated Tailwind CSS into the project for styling and layout purposes.
    - Installed Tailwind CSS via npm and added it as a dependency in the package.json file.
    - Created a configuration file (tailwind.config.js) to customize the default styles, colors, and spacing options.
    - Created the main stylesheet (e.g., input.css) to include Tailwind CSS utility classes.
    - Tested and verified that the application's UI still works as expected with the new styles.
    
    Notes:
    - Make sure to run 'npm install' after pulling this commit to install the new dependencies.
    - Review the tailwind.config.js file for any specific customization requirements.

--- App/View/Home/index.php
+++ App/View/Home/index.php
@@ -5,10 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Patika.dev | Twitter Clone</title>
+
+    <link href="public/dist/output.css" rel="stylesheet">
 </head>
 
 <body>
-    <h1>Twitter Clone</h1>
+
+    <h1 class="text-3xl font-bold underline">Twitter Clone</h1>
+
 </body>
 
 </html>
