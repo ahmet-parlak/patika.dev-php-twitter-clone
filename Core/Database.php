f941d3f0150f1a0e724459be29f58f8247305b9e
Ahmet Parlak <ahmet--parlak@outlook.com>
2023-08-06 18:20:49 +0300

    Update DB config, prepare CRUD functions
    - Added database information to config.php for connection setup.
    - Prepared basic CRUD functions under Core/Database.php to handle
      database interactions.

--- Core/Database.php
+++ Core/Database.php
@@ -7,12 +7,11 @@ class Database
 
     public function __construct()
     {
-
         try {
-            $host = 'localhost';
-            $dbname = 'patika_twitter_clone';
-            $username = 'root';
-            $password = '';
+            $host = HOST;
+            $dbname = DB;
+            $username = DB_USERNAME;
+            $password = DB_PASSWORD;
 
             $this->db = new \PDO("mysql:host=$host;dbname=$dbname", "$username", "$password");
             $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
@@ -24,18 +23,96 @@ class Database
             echo "<hr>";
             print_r($e->getMessage());
             echo "<hr>";
-
         }
     }
 
 
-
-    public function query($sql, $fetchAll = false): array
+    public function fetch($sql): array
     {
-        if ($fetchAll == false) {
-            return $this->db->query($sql, \PDO::FETCH_ASSOC)->fetch() ?? [];
-        } else {
-            return $this->db->query($sql, \PDO::FETCH_ASSOC)->fetchAll() ?? [];
+        return $this->db->query($sql, \PDO::FETCH_ASSOC)->fetch() ?? [];
+    }
+
+    public function fetchAll($sql): array
+    {
+        return $this->db->query($sql, \PDO::FETCH_ASSOC)->fetchAll() ?? [];
+    }
+
+
+    public function insert($table, $values): bool
+    {
+        try {
+            $columns = implode(", ", array_keys($values));
+
+            $vals = ':' . implode(", :", array_keys($values));
+
+            $stmt = $this->db->prepare("INSERT INTO $table ($columns) VALUES ($vals)");
+
+            $stmt->execute($values);
+
+            return true;
+
+        } catch (\PDOException $e) {
+            return false;
+        }
+    }
+
+
+    public function update($table, $values, $keys): bool
+    {
+        try {
+            $setClause = '';
+            foreach ($values as $column => $value) {
+                $setClause .= "$column = :$column, ";
+            }
+            $setClause = rtrim($setClause, ', '); // Remove trailing comma and space
+
+            $whereClause = '';
+            foreach ($keys as $whereColumn => $whereValue) {
+                $whereClause .= "$whereColumn = :where_$whereColumn AND ";
+            }
+            $whereClause = rtrim($whereClause, ' AND '); // Remove trailing "AND"
+
+            $sql = $this->db->prepare("UPDATE $table SET $setClause WHERE $whereClause");
+
+            $parameters = array();
+            foreach ($values as $column => $value) {
+                $parameters[":$column"] = $value;
+            }
+            foreach ($keys as $whereColumn => $whereValue) {
+                $parameters[":where_$whereColumn"] = $whereValue;
+            }
+
+            $sql->execute($parameters);
+
+            return true;
+
+        } catch (\PDOException $e) {
+            return false;
+        }
+    }
+
+    public function delete($table, $keys): bool
+    {
+        try {
+            $whereClause = '';
+            foreach ($keys as $column => $value) {
+                $whereClause .= "$column = :$column AND ";
+            }
+            $whereClause = rtrim($whereClause, ' AND '); // Remove trailing "AND"
+
+            $sql = $this->db->prepare("DELETE FROM $table WHERE $whereClause");
+
+            $parameters = array();
+            foreach ($keys as $column => $value) {
+                $parameters[":$column"] = $value;
+            }
+
+            $sql->execute($parameters);
+
+            return true;
+
+        } catch (\PDOException $e) {
+            return false;
         }
     }
 
--- config.php
+++ config.php
@@ -2,4 +2,9 @@
 session_start();
 const BASEDIR = __DIR__;
 const URL = 'http://localhost/patika.dev/php/twitter-clone/';
+
+const HOST = 'localhost';
+const DB = 'patika_twitter_clone';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = '';
 ?>
